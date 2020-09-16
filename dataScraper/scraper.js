f2c6143aca272bb5a4c3645783e6c094af5f45bf
Sachin Pandey <sach0499@gmail.com>
2020-09-16 13:05:06 +0530

    added scraping functionality

---
+++ dataScraper/scraper.js
@@ -0,0 +1,94 @@
+const puppeteer = require('puppeteer')
+
+
+const getTodayDate = () => {
+
+     let today = new Date();
+     const dd = String(today.getDate()).padStart(2, '0');
+     const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+     const yyyy = today.getFullYear();
+
+     today = `${dd}-${mm}-${yyyy}`
+     
+     return today;
+
+}
+
+
+const scrapeData = async () => {
+
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+  await page.goto('https://www.mohfw.gov.in/');
+
+  await page.click('a.open-table');
+  await page.waitFor(5000);
+
+
+ const stateWiseData = await page.evaluate(()=> {
+ 
+  const rows = document.querySelectorAll('table.statetable tbody tr');
+  const titles = ['serialNo.' ,'name', 'totalActive', 'increaseActive', 'totalRecovered', 'incrreaseRecovered', 'totalDeaths', 'increaseDeaths'];
+  const data = []; 
+
+
+  // this gets the statewiseData
+
+  for(let i = 0; i < 35; i++){
+
+    const state = {};
+    const row = rows[i];
+    const cols = row.querySelectorAll('td');
+
+    cols.forEach((col, index)=> {
+
+
+         if(index === 1)
+             state[titles[index]] = col.textContent;
+         
+        else if(index === 3 || index === 5 || index === 7){
+
+
+             let data = parseInt(col.textContent) || 0;
+
+             if(col.querySelector('.down') !== null)
+                    data *= -1;
+             
+             state[titles[index]] = data;
+        }
+
+        else if(index === 2 || index === 4 || index === 6){
+
+           state[titles[index]] = parseInt(col.textContent);
+
+        }
+
+
+    })
+
+    data.push(state)
+     
+  }
+  
+
+     return data;
+   })
+
+
+
+ await browser.close();
+
+ return {
+
+     todayDate: getTodayDate(),
+     stateWiseData
+ };
+
+
+}
+
+
+
+module.exports = scrapeData;
+

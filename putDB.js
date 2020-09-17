91f7c7fcf778f27f5803785c65ea675a14faac73
Sachin Pandey <sach0499@gmail.com>
2020-09-17 11:42:40 +0530

    loaded data into DB

---
+++ putDB.js
@@ -0,0 +1,121 @@
+// const History = require("./models/History");
+// const mongoose = require('mongoose')
+
+// mongoose.connect("mongodb://localhost:27017/corona-virus-db-v2", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const dataArray = [
+//   {
+//     createdOn: "8-09-2020",
+//     aggregateData: {
+//       totalActive: 882752,
+//       increaseActive: -203,
+//       totalRecovered: 3321425,
+//       increaseRecovered: 74116,
+//       totalDeaths: 72813,
+//       increaseDeaths: 1129,
+//     },
+//   },
+//   {
+//     createdOn: "9-09-2020",
+//     aggregateData: {
+//       totalActive: 896887,
+//       increaseActive: 14135,
+//       totalRecovered: 3396033,
+//       increaseRecovered: 74608,
+//       totalDeaths: 73920,
+//       increaseDeaths: 1107,
+//     },
+//   },
+//   {
+//     createdOn: "10-09-2020",
+//     aggregateData: {
+//       totalActive: 918193,
+//       increaseActive: 21306,
+//       totalRecovered: 3469095,
+//       increaseRecovered: 73062,
+//       totalDeaths: 75088,
+//       increaseDeaths: 1168,
+//     },
+//   },
+//   {
+//     createdOn: "11-09-2020",
+//     aggregateData: {
+//       totalActive: 942803,
+//       increaseActive: 24610,
+//       totalRecovered: 3539999,
+//       increaseRecovered: 70904,
+//       totalDeaths: 76301,
+//       increaseDeaths: 1213,
+//     },
+//   },
+//   {
+//     createdOn: "12-09-2020",
+//     aggregateData: {
+//       totalActive: 957795,
+//       increaseActive: 14992,
+//       totalRecovered: 3621455,
+//       increaseRecovered: 81456,
+//       totalDeaths: 77503,
+//       increaseDeaths: 1202,
+//     },
+//   },
+//   {
+//     createdOn: "13-09-2020",
+//     aggregateData: {
+//       totalActive: 973217,
+//       increaseActive: 15422,
+//       totalRecovered: 3699317,
+//       increaseRecovered: 77862,
+//       totalDeaths: 78614,
+//       increaseDeaths: 1111,
+//     },
+//   },
+//   {
+//     createdOn: "14-09-2020",
+//     aggregateData: {
+//       totalActive: 987539,
+//       increaseActive: 14322,
+//       totalRecovered: 3777065,
+//       increaseRecovered: 77478,
+//       totalDeaths: 1140,
+//       increaseDeaths: 1290,
+//     },
+//   },
+//   {
+//     createdOn: "15-09-2020",
+//     aggregateData: {
+//       totalActive: 989171,
+//       increaseActive: 1632,
+//       totalRecovered: 3856213,
+//       increaseRecovered: 79213,
+//       totalDeaths: 80808,
+//       increaseDeaths: 1054,
+//     },
+//   },
+//   {
+//     createdOn: "16-09-2020",
+//     aggregateData: {
+//       totalActive: 995933,
+//       increaseActive: 5872,
+//       totalRecovered: 3942360,
+//       increaseRecovered: 82961,
+//       totalDeaths: 82066,
+//       increaseDeaths: 1290,
+//     },
+//   },
+//   {
+//     createdOn: "17-09-2020",
+//     aggregateData: {
+//       totalActive: 1009976,
+//       increaseActive: 14043,
+//       totalRecovered: 4025079,
+//       increaseRecovered: 82719,
+//       totalDeaths: 83198,
+//       increaseDeaths: 1132,
+//     },
+//   },
+// ];
+
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const { getStatesData, getHistory } = require("./routes");
+const putDataIntoDatabase = require('./dataScraper/putData')
 
 mongoose.connect("mongodb://localhost:27017/corona-virus-db-v2", {
   useNewUrlParser: true,
@@ -16,3 +17,7 @@ app.get("/api/v1/states", getStatesData);
 app.get("/api/v1/histories", getHistory);
 
 app.listen(PORT, () => console.log(`Server has started at ${PORT}`));
+
+
+setInterval(putDataIntoDatabase, 24*3600*1000);
+

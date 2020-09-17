ff5b08c8ac573f4b50c2138b49976c2b6f485063
Sachin Pandey <sach0499@gmail.com>
2020-09-17 10:25:01 +0530

    added history route handler

--- routes.js
+++ routes.js
@@ -1,18 +1,42 @@
-const {State} = require("./models/State");
+const { State } = require("./models/State");
+const History = require("./models/History");
 
 exports.getStatesData = async (req, res) => {
+  try {
+    const sortParam = req.query.sort || "name";
 
-  const sortParam = req.query.sort || "name";
+    const statesData = await State.find({})
+      .sort(sortParam)
+      .select(["-_id", "-__v"]);
 
-  const statesData = await State.find({}).sort(sortParam);
-
-  res.status(200).json({
-
-    massage: 'success',
-    data: statesData
-  })
- 
-
+    res.status(200).json({
+      status: "success",
+      data: statesData,
+    });
+  } catch {
+    res.status(201).json({
+      status: "failure",
+      message: "Try Again Later!",
+    });
+  }
 };
 
-exports.getHistory = (req, res) => {};
+exports.getHistory = async (req, res) => {
+  try {
+    const limit = parseInt(req.query.limit) || 10;
+
+    const histories = await History.find({})
+      .limit(limit)
+      .select(["-_id", "-__v"]);
+
+    res.status(200).json({
+      message: "success",
+      data: histories,
+    });
+  } catch {
+    res.status(201).json({
+      status: "failure",
+      message: "Try Again Later!",
+    });
+  }
+};

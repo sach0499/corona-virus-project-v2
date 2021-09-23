4aef350d87f75f43e139d8d448cf6a167165af47
Sachin Pandey <sach0499@gmail.com>
2021-09-23 10:02:02 +0530

    Removed Unnecessary packages

--- server.js
+++ server.js
@@ -1,42 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const rateLimit = require("express-rate-limit");
-const helmet = require("helmet");
-const cors = require("cors")
-const mongoSanitize = require("express-mongo-sanitize");
-const xss = require("xss-clean");
-const compression = require("compression");
+const cors = require("cors");
 
 const { getStatesData, getHistory } = require("./routes");
 const putDataIntoDatabase = require("./dataScraper/putData");
 
-const { put } = require("request-promise");
-
 const app = express();
 
-// Set security HTTTP headers
-app.use(helmet());
-
-// Limit requests from same IP
-const limiter = rateLimit({
-  max: 1000,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many request from the same IP. Please try again in one hour.",
-});
-
-app.use("/api", limiter);
-
-// Data Sanitization against NoSQL query injection
-app.use(mongoSanitize());
-
-// Data Sanitization against XSS attacks
-app.use(xss());
-
-// for CORS
-app.use(cors())
-
-// for compressing the text responses to client
-app.use(compression());
+app.use(cors());
 
 mongoose.connect(
   `mongodb+srv://KillerSachin1999:Remu%24lup1n@cluster0-ntjqw.mongodb.net/corona-virus-db?retryWrites=true&w=majority`,
